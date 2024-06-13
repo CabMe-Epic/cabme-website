2aa23387dfa66c8674d19d3b082dc4f03ea20560
1RahulP <007rahulpundir@gmail.com>
2024-06-13 16:42:30 +0530

    changes

--- src/app/components/specifications/specifications.tsx
+++ src/app/components/specifications/specifications.tsx
@@ -2,8 +2,15 @@ const Specifications = () =>{
     return(
         <div className="p-4">
             <h2 className="text-2xl font-semibold border-b pb-2">Specifications</h2>
-
+            <div className="grid grid-cols-4">
+                {}
+            </div>
         </div>
     )
 }
 export default Specifications
+const specificationArray = [
+    {
+        imageUrl:""
+    }
+]
