7e756fe42b27b0edea3654d1ab441564c1b78691
1RahulP <007rahulpundir@gmail.com>
2024-07-09 16:31:35 +0530

    car listing page

---
+++ src/app/components/exclusion/exclusion.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+
+const ExclusionComponent = () =>{
+    return(
+
+        <div>
+            <div>
+                <Image src={} alt="" width={16} height={16} />
+            </div>
+            <span></span>
+        </div>
+    )
+}
+export default ExclusionComponent
