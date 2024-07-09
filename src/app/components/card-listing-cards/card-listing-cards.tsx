c6525f429c6c7f176e4820a75e2302eca657af95
suraj dubey <suraj.dubey@epicglobal.co.in>
2024-07-09 13:13:26 +0530

    car listing

--- src/app/components/card-listing-cards/card-listing-cards.tsx
+++ src/app/components/card-listing-cards/card-listing-cards.tsx
@@ -172,12 +172,13 @@ const CardListingCards = ({ data }: any) => {
                       </p>
                     </span>
                   </div>
-                  <div onClick={() => {setPrice(data?.bookingOptions?.selfDrive?.packageType?.package2
-                    ?.price);
+                  <div onClick={() => {
+                    setPrice(data?.bookingOptions?.selfDrive?.packageType?.package2
+                      ?.price);
                     setClicked1(false);
                     setClicked2(true);
                     setClicked3(false);
-                    }} className={`sm:flex flex-row items-center justify-between bg-white gap-3 border-[1.5px] border-[#FF0000] px-2 py-2 rounded-lg sm:w-[210px] sm:h-[71px] cursor-pointer ${clicked2 ? "border-black bg-gradient-to-r from-[#FFD7D7] transition-all  to-[#fff]" : ""}`}>
+                  }} className={`sm:flex flex-row items-center justify-between bg-white gap-3 border-[1.5px] border-[#FF0000] px-2 py-2 rounded-lg sm:w-[210px] sm:h-[71px] cursor-pointer ${clicked2 ? "border-black bg-gradient-to-r from-[#FFD7D7] transition-all  to-[#fff]" : ""}`}>
                     <span className="font-bold text-[18px] ">
                       {
                         data?.bookingOptions?.selfDrive?.packageType?.package2
@@ -198,8 +199,9 @@ const CardListingCards = ({ data }: any) => {
                     </span>
                   </div>
                   <div
-                    onClick={() => {setPrice(data?.bookingOptions?.selfDrive?.packageType?.package3
-                      ?.price);
+                    onClick={() => {
+                      setPrice(data?.bookingOptions?.selfDrive?.packageType?.package3
+                        ?.price);
                       setClicked1(false);
                       setClicked2(false);
                       setClicked3(true);
