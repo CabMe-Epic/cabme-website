f49aa0794f01abc446f65f72c0823eed5d0ae964
SahilMishraDev <msahil76640@gmail.com>
2024-08-01 09:05:24 +0530

    payment

---
+++ src/app/(pages)/failure/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Failure = () => {
+  return (
+    <div>
+      <h1>Payment Failure</h1>
+      <p>
+        Your payment has been Failure processed. Thank you for your purchase!
+      </p>
+    </div>
+  );
+};
+
+export default Failure;
