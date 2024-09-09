18d77570197070927025faf8d810e4a14e264cfe
suraj dubey <dubeysuraj864@gmail.com>
2024-09-09 17:37:22 +0530

    changes

--- networkRequests/hooks/useVehicles.tsx
+++ networkRequests/hooks/useVehicles.tsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 import "../types/type";
 
 
-const useVehicles = (): UseVehiclesResult => {
-  const [vehicles, setVehicles] = useState<Vehicle[] >([]);
+const useVehicles = (): any => {
+  const [vehicles, setVehicles] = useState<any[] >([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<Error | any>(null);
 
@@ -13,7 +13,7 @@ const useVehicles = (): UseVehiclesResult => {
       console.log('URI_BASE:', process.env.NEXT_PUBLIC_URI_BASE);
 
       try {
-        const response = await axios.get<Vehicle[] | any>(`${process.env.NEXT_PUBLIC_URI_BASE}/cabme/vehicles`);
+        const response = await axios.get<any[] | any>(`${process.env.NEXT_PUBLIC_URI_BASE}/cabme/vehicles`);
         setVehicles(response.data);
         setLoading(false);
       } catch (err) {
