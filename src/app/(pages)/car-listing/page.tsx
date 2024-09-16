28a7e758b95d2845bf6056bebb8d760a014a0872
suraj dubey <dubeysuraj864@gmail.com>
2024-09-16 19:15:22 +0530

    changes

--- src/app/(pages)/car-listing/page.tsx
+++ src/app/(pages)/car-listing/page.tsx
@@ -319,22 +319,8 @@ const CarListing = () => {
     const getCarDetails = async () => {
       try {
         if (
-          (bookingOptions === "Self-Driving")
-        ) {
-          if (
-            locationData.pickUpDateTime &&
-            (bookingOptions)
-          ) {
-            console.log("locationData", locationData);
-            setLoader(true);
-            const getSearchCarData = await searchVehicleNew(locationData);
-            console.log(getSearchCarData, "Car search API response");
-            setCarData(getSearchCarData?.data?.availableVehicles);
-          } else {
-            console.error("Missing required location data or booking options", locationData);
-          }
-        }
-        if (
+          bookingOptions === "Self-Driving" ||
+       
           (bookingOptions === "Driver" &&
             (driverType === "One-way"))
         ) {
