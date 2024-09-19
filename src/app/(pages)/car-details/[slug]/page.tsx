b41fe92e952984efe8175f97530038e3440a6a8a
suraj dubey <dubeysuraj864@gmail.com>
2024-09-19 13:33:50 +0530

    changes

--- src/app/(pages)/car-details/[slug]/page.tsx
+++ src/app/(pages)/car-details/[slug]/page.tsx
@@ -644,7 +644,8 @@ const CarDetails = () => {
   return (
     <>
       <div className="py-6">
-        <ToastContainer />
+      <div className="z-[99999]"><ToastContainer /></div>
+
         <div className="sm:flex hidden px-16 text-[#5F5D5D]">
           <span className="cursor-pointer">Home</span>/
           <span className="cursor-pointer">Listing</span>/
