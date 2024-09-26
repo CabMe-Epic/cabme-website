764722418b32d53e0384c0b4c866dcea708d1382
suraj dubey <dubeysuraj864@gmail.com>
2024-09-26 12:40:08 +0530

    changes

--- src/app/(pages)/failure/page.tsx
+++ src/app/(pages)/failure/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Failure = () => {
   return (
-    <div>
+    <div className="flex justify-center items-center w-full text-center h-[50vh]">
       <h1>Payment Failure</h1>
       <p>
         Your payment has been Failure processed. Thank you for your purchase!
