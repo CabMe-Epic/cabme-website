101ae0dbfccd218f2f3c8eec4cc6e36cbe56636a
suraj dubey <dubeysuraj864@gmail.com>
2024-11-06 18:32:56 +0530

    changes in loader

--- src/app/components/blinker-loader/blinkerLoader.tsx
+++ src/app/components/blinker-loader/blinkerLoader.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 
 const BlinkerLoader = () => {
   return (
-    <div className="flex fixed left-0 top-0 z-[10000] items-center justify-center h-screen w-screen bg-gray-100">
-      <div className="animate-blink">
-        <Image src="/logo.svg" alt="Logo" width={200} height={200} />
+    <div className="flex fixed left-0 top-0 z-[10000] items-center justify-center h-screen w-screen bg-[#0000009f]">
+      <div className="animate-spin">
+        <Image src="/loaderRound.png" alt="Logo" width={50} height={50} />
       </div>
     </div>
   );
